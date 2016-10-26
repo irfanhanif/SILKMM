33a158d671323935990e3ac9ca9c6531f2e37410
nafiarwhite <nafiar21@gmail.com>
2016-10-26 20:38:54 +0700

    undo takdir

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 
 class homeController extends Controller
 {
     //
-//    public function show(){
+    public function show(){
+    	$data_pelatihan = DB::table('pelatihan')->get();
 
-//    }
+//    	echo $data_pelatihan;
+    	return $data_pelatihan;
+    }
 }
