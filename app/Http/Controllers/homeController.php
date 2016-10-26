3e53dd49c1f7dcceef626b19e2c37c35357532c8
nafiarwhite <nafiar21@gmail.com>
2016-10-27 00:36:53 +0700

    controller

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Requests;
+use App\Http\Controllers\Controller;
+//use App\pelatihan;
 
 class homeController extends Controller
 {
@@ -12,7 +13,22 @@ class homeController extends Controller
     public function show(){
     	$data_pelatihan = DB::table('pelatihan')->get();
 
-//    	echo $data_pelatihan;
     	return $data_pelatihan;
+    }
+
+    public function daftar(Request $request){
+    	$nama_pelatihan = $request->nama_pelatihan;
+    	$no_pelatihan = $request->no_pelatihan;
+
+    	return view('pendaftaran.register', [
+    		'nama_p elatihan' => $nama_pelatihan,
+    		'no_paletihan' => $no_pelatihan]);
+    }
+
+    public function status(Request $request){
+    	$no_pelatihan = $request->no_pelatihan;
+    	$berkas = DB::table('berkas')->where('id_pelatihan',$no_pelatihan)->get();
+
+    	return view('pendaftaran.register', ['berkas' => $berkas]);
     }
 }
--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -11,25 +11,33 @@ class userController extends Controller
 {
     public function show(Request $request){
     	$berkas = new Berkas;
-       	//ID Berkas
-    	$id_berkas = DB::table('berkas')->max('id_berkas');
+       	$id_berkas = DB::table('berkas')->max('id_berkas');
+        $nrp = $request->$nrp;
+        $no_pelatihan = $request->no_pelatihan;
+        $status_pelatihan = DB::table('pelatihan')->where('id_pelatihan',$no_pelatihan)->value('syarat_berkas');
+        $tgl_lahir = $request->tgl_lahir;
+
+        //ID Berkas
     	$berkas->id_berkas = ($id_berkas+1);
+        $berkas->id_peserta = $nrp;
+    	$berkas->id_pelatihan = $request->no_pelatihan;
 
-        //NRP
-        $berkas->id_peserta = $request->nrp;
+        if(preg_match("#([a-zA-Z0-9-_\\/]+\\.zip)#", $_FILES["lampiran_cv"]["name"], $matches, PREG_OFFSET_CAPTURE)){
+            #continue
+        }
+        else {
+            $error = 2;
+            return view('registration', ['error' => $error]);
+        }
+        $konten_berkas = $request->file('berkas');
+        Storage::disk('lampiran')->put(
+            $nrp.'/'.$nrp.'_berkas.zip',
+            file_get_contents($konten_berkas->getRealPath())
+            );
 
-    	//ID Pelatihan
-    	$id_pelatihan = DB::table('pelatihan')->where('nama_pelatihan',$request->pelatihan)->value('id_pelatihan');
-    	echo $id_pelatihan . "<br>";
-    	$berkas->id_pelatihan = $id_pelatihan;
-
-    	//Path Berkas
-
-
-    	//Status Berkas
-    	$status_pelatihan = DB::table('pelatihan')->where('nama_pelatihan',$request->pelatihan)->value('syarat_berkas');
+        $no_pelatihan = 1;
     	$strlen = strlen($status_pelatihan);
-    	$counter = 0;
+        $counter = 0;
     	$status_berkas = "0";
     	for($i=0; $i<=$strlen; $i++){
     		$char = substr($status_pelatihan, $i, 1);    		
@@ -37,10 +45,12 @@ class userController extends Controller
     			$status_berkas .= ",0";
     		}
     	}
-    	echo $status_berkas . "<br>";
     	$berkas->status_berkas = $status_berkas;
 
-    	//
+        if($tgl_lahir != DB::table('peserta')->where('NRP', $nrp)->value('tgl_lahir'));{
+            $error = 1;
+            return view('pendaftaran.register', $error);
+        }
 
     	$berkas->save();
     	return ;
